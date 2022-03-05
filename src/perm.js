c7036d390dbb8d3dd045901ec4c787f051d83da5
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-05 11:54:23 +0200

    switcharoo, run command again if not registered

--- src/perm.js
+++ src/perm.js
@@ -5,26 +5,27 @@ module.exports = {
        const sperms = util.apis["shark-perms-manager"].api;
        switch(args[0]) {
 		   case 'myperms':
-			   sharkdb.getUser(message.author.id, user => {
-				   if (!user) return message.channel.send('try again');
+				sperms.permittedTo("perm", message.author.id, message.guild.id, permitted => {
+					if(!permitted) return message.channel.send("you not allowed idiot");
+					message.channel.send("you have perm epic man")
+				});
+			   break;
+			default:
+				sharkdb.getUser(message.author.id, user => {
+					if (!user) return this.execute(message, args, util);
 				   let permEmbed = new MessageEmbed()
 				   .setColor("#00A8F3")
 				   .setTitle(`${message.author.username}'s permissions`)
 				   .addFields(
 					user.permissions.permissions.filter(p => p?.global).length > 0 ? { name: 'Global Permissions', value: user.permissions.permissions.filter(p => p?.global).map(p => p.id).join(', ') } : { name: 'Global Permissions', value: "none lol" }, 
-					user.permissions.permissions.filter(p => p?.guildOnly.includes(message.guild.id)).length > 0 ? { name: 'Guild Permissions', value: user.permissions.permissions.filter(p => p?.guildOnly.includes(message.guild.id)).map(p => p.id).join(', ') } : { name: 'Guild Permissions', value: "none lol" },
+					user.permissions.permissions.filter(p => p?.guildOnly.includes(message.guild.id) && !p?.global).length > 0 ? { name: 'Guild Permissions', value: user.permissions.permissions.filter(p => p?.guildOnly.includes(message.guild.id) && !p?.global).map(p => p.id).join(', ') } : { name: 'Guild Permissions', value: "none lol" },
 					user.permissions.groups.length > 0 ? { name: 'Groups', value: user.permissions.groups.map(g => g?.name).join(', ') } : { name: 'Groups', value: "none lol" }
 				   )
 				   .setTimestamp()
 				   .setFooter({text:"shark-perms-manager copyright 2023 urmom reserved", iconURL: "https://cdn.discordapp.com/avatars/585121910388424724/47ceb1466c5bd0648487c784d394cbfd.webp"});
 					message.channel.send({embeds: [permEmbed]});
 				})
-			   break;
-			default:
-				sperms.permittedTo("perm", message.author.id, message.guild.id, permitted => {
-					if(!permitted) return message.channel.send("you not allowed idiot");
-					message.channel.send("you have perm epic man")
-				});
+				break;
 	   }
     }
 }
