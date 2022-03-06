7999b90880c9e987be0b5147a4ab99e662739965
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-06 17:55:16 +0200

    Update perm.js

--- src/perm.js
+++ src/perm.js
@@ -4,6 +4,31 @@ module.exports = {
        const sharkdb = util.apis["shark-db-db"].api;
        const sperms = util.apis["shark-perms-manager"].api;
        switch(args[0]) {
+			case 'group':
+				sperms.permittedTo("perm", message.author.id, message.guild.id, (perm, user) => {
+					if (!perm) return message.channel.send("you not allowed idiot");
+					if (!args[1] || !args[2] || !args[3]) return message.channel.send("usage: perm group [add/remove] [group name] [user id]")
+					if(!/^[0-9]{18,19}$/gm.test(args[3])) return message.channel.send("invalid user id >:(")
+					if(!["add", "remove"].includes(args[1])) return message.channel.send("add or remove")
+					sharkdb.getGroup(args[2], group => {
+						if(!group) return message.channel.send("group not found")
+						let userInGroup = user.permissions.groups.find(g => g._id.toString() == group._id.toString())
+						if(args[1] == "add") {
+							if (userInGroup) return message.channel.send("user already in group")
+							user.permissions.groups.push({_id: group._id, name: group.name})
+						}
+						if(args[1] == "remove") {
+							if (!userInGroup) return message.channel.send("user not in group")
+							user.permissions.groups.splice(user.permissions.groups.indexOf(group._id), 1)
+						}
+						user.markModified("permissions");
+						user.save(err => {
+							if (err) return message.channel.send("error saving user")
+							message.channel.send("user added/removed from/to group")
+						})
+					})
+				})	
+			break;
 		    case 'creategroup':
 				sperms.permittedTo("perm", message.author.id, message.guild.id, permitted => {
 					if(!permitted) return message.channel.send("you not allowed idiot");
